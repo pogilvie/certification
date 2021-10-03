e790d889f5ad2be062ed2f75dab0fe5c5f848b35
pogilvie <code@ogilvie.us.com>
2021-10-03 16:43:04 -0700

    done

--- javascript-dev1/jest.config.js
+++ javascript-dev1/jest.config.js
@@ -2,5 +2,8 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
+    moduleNameMapper: {
+      '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
+    }
 };
